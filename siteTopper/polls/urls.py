b8c8be81544f440e0de3869e13fcccb49fecd6d4
LeeonardoVargas <leu1607@hotmail.com>
2018-10-24 11:01:37 -0300

    Algumas alterações

--- siteTopper/polls/urls.py
+++ siteTopper/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('polls/', views.index, name='index'),
+    path('', views.index, name='index'),
     # ex: /polls/5/
-    path('polls/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('polls/<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- siteTopper/polls/views.py
+++ siteTopper/polls/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-
 
 def index(request):
     questoes = Question.objects.order_by('-pub_date')[:2]
@@ -12,7 +10,8 @@ def index(request):
     return render(request, template, context)
 
 def detail(request, question_id):
-    return HttpResponse("Veja a minha questão %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
