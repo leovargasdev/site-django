be0dafc21e7a6ca29998a1212b38d8367fbbcfd9
LeeonardoVargas <leu1607@hotmail.com>
2018-10-31 10:58:48 -0300

    Estruturando novo modulo

--- siteTopper/blog/views.py
+++ siteTopper/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Piada
+
+def cadastro(request):
+    if(request.method == 'POST'):
+        inputs = request.POST
+        p = Piada()
+        p.titulo = inputs['titulo']
+        p.conteudo = inputs['conteudo']
+        p.categoria = inputs['categoria']
+        p.save()
+        return redirect('/blog')
+    context = ""
+    template = 'cadastro_piada.html'
+    return render(request, template, None)
+
+def index(request):
+    result = Piada.objects
+    print (result)
+    # for r in result:
+    #     print(r)
+    perdi = [{'titulo': "abacaxi"}, {'titulo': "teste"}, {'titulo': "carro"}, {'titulo': "super"}]
+    context = {'piadas': perdi}
+    template = 'index.html'
+    return render(request, template, context)
